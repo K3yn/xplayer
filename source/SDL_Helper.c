80300f38fe7f9c4c2cff441d33b089f33c6f8141
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-18 21:24:52 +0100

    Update SDL_Helper.c

--- source/SDL_Helper.c
+++ source/SDL_Helper.c
@@ -1,1 +1,74 @@
+//#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfxPrimitives.h> 
+#include "xplayer_fonts.h"
+#include "SDL_helper.h"
 
+void SDL_ClearScreen(SDL_Renderer *renderer, SDL_Color colour)
+{
+	SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a);
+	SDL_RenderClear(renderer);
+}
+
+void SDL_LoadImage(SDL_Texture** texture, u8* buff, size_t size)
+{
+    SDL_Surface* loaded_surface = NULL;
+    loaded_surface = IMG_Load_RW(SDL_RWFromMem(buff, size), 1);
+
+    if (loaded_surface)
+    {
+        Uint32 colorkey = SDL_MapRGB(loaded_surface->format, 0, 0, 0);
+        SDL_SetColorKey(loaded_surface, SDL_TRUE, colorkey);
+        *texture = SDL_CreateTextureFromSurface(renderer, loaded_surface);
+    }
+
+    SDL_FreeSurface(loaded_surface);
+}
+void SDL_DrawImageScale(SDL_Texture* texture, int x, int y, int w, int h)
+{
+    SDL_Rect position;
+    position.x = x; position.y = y; position.w = w; position.h = h;
+    SDL_RenderCopy(renderer, texture, NULL, &position);
+}
+void SDL_DrawRect(SDL_Renderer *renderer, int x, int y, int w, int h, SDL_Color colour)
+{
+	SDL_Rect rect;
+	rect.x = x; rect.y = y; rect.w = w; rect.h = h;
+	SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, colour.a);
+	SDL_RenderFillRect(renderer, &rect);
+}
+void SDL_DrawCircle(SDL_Renderer *renderer, int x, int y, int r, SDL_Color colour)
+{
+	filledCircleRGBA(renderer, x, y, r, colour.r, colour.g, colour.b, colour.a);
+	return;
+}
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int Srcx, int Srcy, int Destx, int Desty, int w, int h)
+{
+	SDL_Rect srce;
+	srce.x = Srcx;
+	srce.y = Srcy;
+	srce.w = w;
+	srce.h = h;
+
+	SDL_Rect dest;
+	dest.x = Destx;
+	dest.y = Desty;
+	dest.w = w;
+	dest.h = h;
+
+	SDL_RenderCopy(ren, tex, &srce, &dest);
+}
+void SDL_DrawText(SDL_Renderer *renderer, TTF_Font *font, int x, int y, SDL_Color colour, const char *text)
+{
+	SDL_Surface *surface = TTF_RenderText_Blended_Wrapped(font, text, colour, 1280);
+	SDL_SetSurfaceAlphaMod(surface, colour.a);
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+	
+	SDL_Rect position;
+	position.x = x; position.y = y;
+	SDL_QueryTexture(texture, NULL, NULL, &position.w, &position.h);
+	SDL_RenderCopy(renderer, texture, NULL, &position);
+	SDL_DestroyTexture(texture);
+}
