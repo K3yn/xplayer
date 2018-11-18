51f65a830edef7eaf7f2a159b273db5c4666d942
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-18 21:27:30 +0100

    Update xplayer_touch.c

--- source/xplayer_touch.c
+++ source/xplayer_touch.c
@@ -1,1 +1,19 @@
+#include <switch.h>
+#include "xplayer_touch.h"
+
+bool inBox(touchPosition touch, int x1, int y1, int x2, int y2)
+{
+	int tx=touch.px;
+	int ty=touch.py;
+
+	if (tx > x1 && tx < x2 && ty > y1 && ty < y2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
 
