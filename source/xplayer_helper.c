c29605e783029d004793be8acacb10d9a329e68b
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-18 21:26:55 +0100

    Update xplayer_helper.c

--- source/xplayer_helper.c
+++ source/xplayer_helper.c
@@ -1,1 +1,25 @@
+#include <SDL2/SDL.h>
+//#include <SDL2/SDL_image.h>
+//#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+#include "SDL_helper.h"
+#include "xplayer_touch.h"
+#include <time.h>
+#include "xplayer_helper.h"
+
+
+void loadIniVars(){
+
+	mode_main_menu=true;
+	mode_play_video=false;
+	mode_display_debug=false;
+	contMenuMain=0;
+	selector=0;
+	selectorVideo=0;
+}
+
+void resetVars(){
+
+}
+
 
