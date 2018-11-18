b2e52130e6ecf180f0eb0cfd56aba89f50291e0a
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-18 21:25:30 +0100

    Update xplayer_fonts.c

--- source/xplayer_fonts.c
+++ source/xplayer_fonts.c
@@ -1,1 +1,15 @@
+#include <switch.h>
+//#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include "xplayer_fonts.h"
 
+//
+void load_fonts()
+{
+	Roboto = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 25);
+	Roboto_large = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 35);
+	Roboto_50 = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 50);
+	Roboto_60 = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 60);
+	Roboto_80 = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 80);
+	Roboto_max = TTF_OpenFont("romfs:/resources/Roboto-Regular.ttf", 100);
+}
