11dd9a0f7773ff710518f617a031e3b244963bb9
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-18 21:29:55 +0100

    Update xplayer_helper.h

--- include/xplayer_helper.h
+++ include/xplayer_helper.h
@@ -1,1 +1,22 @@
+#ifndef xplayer_helper_H
+#define xplayer_helper_H
 
+u32 kDown;
+u32 kHeld;
+u32 kUp;
+
+
+bool mode_main_menu;
+bool mode_display_media_dir;
+bool mode_play_video;
+bool mode_display_debug;
+int contMenuMain;
+int selector;
+int selectorVideo;
+char charSelector[10];
+char charSelectorVideo[10];
+
+void loadIniVars();
+void resetVars();
+
+#endif
