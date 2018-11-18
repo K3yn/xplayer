339ad706782a2a0e5b7f008b199600ef484cceb3
K3yn <44470230+K3yn@users.noreply.github.com>
2018-11-18 21:29:04 +0100

    Update xplayer_fs.h

--- include/xplayer_fs.h
+++ include/xplayer_fs.h
@@ -1,1 +1,16 @@
+#ifndef xplayer_fs_H
+#define simon_savegame_H
+
+#define MEDIADIR "sdmc:/switch/media/"
+
+struct stat s;
+int err;
+const char* savepath;
+char linea[100];// 
+char nameVideo[15][50];
+int contFilesInMedia;
+
+void leeDirMedia();
+
+#endif
 
